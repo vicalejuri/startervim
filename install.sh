0d5fd847e8aea90df046a2d073c00966ee7e11bb
Chas. J. Owens IV <chas.owens@gmail.com>
2011-06-26 00:12:27 -0400

    provide useful default files for common file types

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Baking up old files
 echo "Saving old files..."
-for file in ~/.vim ~/.vimrc ~/.vimrc-keymaps; do
+for file in ~/.vim ~/.vimrc ~/.vimrc-keymaps ~/.vimrc-au; do
 	if [ -h file ]; then
 		echo "baking up your .vim folder"
 		mv file "$file.`date +%s`.bak" 
@@ -14,6 +14,7 @@ echo "Linking .vim , .vimrc and .vimrc-keymaps to startervim"
 ln -s `pwd`/vim ~/.vim
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/vimrc-keymaps ~/.vimrc-keymaps
+ln -s `pwd`/vimrc-au ~/.vimrc-au
 
 echo "Installing bundles..."
 ruby `pwd`/vim/bin/vim-update-bundles.rb
